3179b07ab7e7855cfcf8033975168adf83ec8d5a
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-11-12 08:59:06 -0800

    coded Description component

---
+++ src/components/Description.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return (
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className="highlight_description">
+                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
